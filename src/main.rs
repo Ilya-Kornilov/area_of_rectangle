2f5083ad1702f1370c881d2786a2effb627ae5f1
Ilya-Kornilov <ilya.kornilov1987@gmail.com>
2024-12-07 15:44:03 +0300

    another two fn added to calculate an area

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,40 @@
+struct Rectangle {
+    width: u32,
+    height: u32
+}
+
 fn main() {
 
     let width = 30;
     let height = 50;
     
     println!(
-        "The area of the rectangle is {} square pixels",
+        " > 1: 
+    >> the area of the rectangle is {} square pixels",
         area(width, height)
     );
+
+
+    let rect = (width, height);
+
+    println!(
+        " > 2: 
+    >> the area of the rectangle is {} square pixels",
+        area_2(rect)
+    );
+
+    let rect_3 = Rectangle {
+        width: 30, 
+        height: 50
+    };
+
+    println!(
+        " > 3: 
+    >> the area of the rectangle is {} square pixels",
+        area_3(&rect_3)
+    );
+
+
 }
 
 fn area(width: u32, height: u32) -> u32
@@ -14,4 +42,10 @@ fn area(width: u32, height: u32) -> u32
     width * height
 }
 
-// video 5, 7'16"
+fn area_2(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
+}
+
+fn area_3(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
